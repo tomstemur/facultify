005659febdad55b0259780bd84d613bfff5758f3
Thomas Murphy <32482407+tomstemur@users.noreply.github.com>
2019-09-10 13:02:11 +0100

    Add files via upload

---
+++ facultify.js
@@ -0,0 +1,14 @@
+var count = [];
+var match = ["05","09","22","26","27","29","35","62","66","69","35"];
+
+$(".popupcardhover").each( function() {
+  var costCode = $(this).text().substring(0,2);
+  if (match.includes(costCode)) {
+    $(this).parents(".record").css("background-color","LightBlue");
+    count++;
+  } else if (!isNaN(costCode)) {
+    $(this).parents(".record").css("display","none");
+  }
+});
+
+$(".brand").text("Science Contracts (" + count + ")");
