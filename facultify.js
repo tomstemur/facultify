8b3635195384968df5ad5a5abd75097e750939a2
Thomas Murphy <32482407+tomstemur@users.noreply.github.com>
2022-02-17 15:15:58 +0000

    Update facultify.js

--- facultify.js
+++ facultify.js
@@ -1,5 +1,5 @@
 var count = 0;
-var match = ["05","09","22","26","27","29","35","62","66","69","35","46"];
+var match = ["05","09","22","26","27","29","35","62","66","69","35","46","85"];
 
 $(".popupcardhover").each( function() {
   var costCode = $(this).text().substring(0,2);
