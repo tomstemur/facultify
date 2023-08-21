854defe53d947187abe50161eee7dc12e4bf73d5
Thomas Murphy <32482407+tomstemur@users.noreply.github.com>
2023-08-21 10:22:39 +0100

    Update facultify.js

--- facultify.js
+++ facultify.js
@@ -1,9 +1,9 @@
 var count = 0;
-var match = ["05**","09**","22**","26**","27**","29**","35**","62**","66**","69**","35**","46**","4981","85**"];
+var match = ["05","09","22","26","27","29","35","62","66","69","35","46","49","85"];
 
 $(".popupcardhover").each( function() {
-  var costCode = $(this).text().substring(0,4);
-  if (match.includes(costCode)) {
+  var costCode = $(this).text().substring(0,2);
+  if (match.includes(costCode) && $(this).text().substring(0,4)!="4982") {
     $(this).parents(".record").css("background-color","LightBlue");
     count++;
   } else if (!isNaN(costCode)) {
