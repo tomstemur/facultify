43c3d2de7d4285c781c44eb6f1456b11917035b9
Thomas Murphy <32482407+tomstemur@users.noreply.github.com>
2024-06-03 10:40:03 +0100

    Update facultify.js
    amending to remove jquery reliance

--- facultify.js
+++ facultify.js
@@ -1,13 +1,13 @@
 var count = 0;
 var match = ["05","09","22","26","27","29","35","62","66","69","35","46","49","85"];
 
-$(".popupcardhover").each( function() {
-  var costCode = $(this).text().substring(0,2);
-  if (match.includes(costCode) && $(this).text().substring(0,4)!="4982") {
+document.getElementById(".popupcardhover").each( function() {
+  var costCode = document.getElementById(this).text().substring(0,2);
+  if (match.includes(costCode) && document.getElementById(this).text().substring(0,4)!="4982") {
     count++;
   } else if (!isNaN(costCode)) {
-    $(this).parents(".record").css("display","none");
+    document.getElementById(this).parents(".record").css("display","none");
   }
 });
 
-$(".brand").text("Science Contracts (" + count + ")");
+document.getElementById(".brand").text("Science Contracts (" + count + ")");
