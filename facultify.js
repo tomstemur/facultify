65f18cb9bb34232cf048d6a20ad6749f9cffceda
Thomas Murphy <32482407+tomstemur@users.noreply.github.com>
2024-12-02 16:04:03 +0000

    Update facultify.js

--- facultify.js
+++ facultify.js
@@ -1,11 +1,36 @@
 var count = 0;
 var match = ["05","09","22","26","27","29","35","62","66","69","35","46","49","85"];
+var list = [
+"Agricultural and Environmental Sciences",
+"Animal Sciences",
+"Biosciences Central",
+"Food, Nutrition and Dietetics",
+"Microbiology, Brewing and Biotechnology",
+"Plant and Crop Sciences",
+"Chemistry",
+"Computer Science Dept",
+"Horizon",
+"Mathematical Sciences Dept",
+"Pharmacy Dept",
+"Physics and Astronomy Dept",
+"Psychology Dept",
+"Science Central Dept",
+"Digital Research Service Central",
+"Global Engagement Support",
+"Governance and Executive Support",
+"Knowledge Exchange and Business Engagement",
+"Reearch Operations",
+"Research and Innovation Central",
+"Research and Innovation Strategy",
+"Vice Chancellor's Office Central Dept"
+];
 
 $(".popupcardhover").each( function() {
   var costCode = $(this).text().substring(0,2);
-  if (match.includes(costCode) && $(this).text().substring(0,4)!="4982") {
+  var newCode = $(this).text();
+  if (match.includes(costCode) || list.includes(newCode)) {
     count++;
-  } else if (!isNaN(costCode)) {
+  } else {
     $(this).parents(".record").css("display","none");
   }
 });
